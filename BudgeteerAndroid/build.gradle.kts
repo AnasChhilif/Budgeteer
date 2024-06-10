e0e65fdae8dfa9ba26813178601596cda4a98b1e
Saad Beidouri <saad.beidouri@etu.unice.fr>
2024-06-10 16:05:58 +0200

    init menu bar

--- BudgeteerAndroid/build.gradle.kts
+++ BudgeteerAndroid/build.gradle.kts
@@ -39,6 +39,9 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation(projects.shared)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
--- BudgeteerAndroid/src/main/java/com/budgeteer/budgeteer/android/MainActivity.kt
+++ BudgeteerAndroid/src/main/java/com/budgeteer/budgeteer/android/MainActivity.kt
@@ -3,11 +3,29 @@ package com.budgeteer.budgeteer.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import  androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.budgeteer.budgeteer.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -15,12 +33,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
+                val navController = rememberNavController()
+
+                val tabBarItems = listOf(
+                    TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home),
+                    TabBarItem(title = "Alerts", selectedIcon = Icons.Filled.Notifications, unselectedIcon = Icons.Outlined.Notifications),
+                    TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings),
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingView(Greeting().greet())
+
                 }
+                BottomNavigationBar(navController, tabBarItems)
             }
         }
     }
@@ -35,6 +62,50 @@ fun GreetingView(text: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
+        val tabBarItems = listOf(
+            TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home),
+            TabBarItem(title = "Alerts", selectedIcon = Icons.Filled.Notifications, unselectedIcon = Icons.Outlined.Notifications),
+            TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings),
+        )
         GreetingView("Hello, Android!")
+        BottomNavigationBar(navController = rememberNavController(), tabBarItems =tabBarItems )
     }
 }
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
+
+@Composable
+fun BottomNavigationBar(navController: NavController, tabBarItems: List<TabBarItem>) {
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        NavigationBar {
+            tabBarItems.forEachIndexed { index, tabBarItem ->
+                NavigationBarItem(
+                    selected = selectedTabIndex == index,
+                    onClick = {
+                        selectedTabIndex = index
+                        //navController.navigate(tabBarItem.title)
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = if (selectedTabIndex == index) tabBarItem.selectedIcon else tabBarItem.unselectedIcon,
+                            contentDescription = tabBarItem.title
+                        )
+                    },
+                    label = {
+                        Text(text = tabBarItem.title)
+                    }
+
+                )
+            }
+        }
+    }
+}
+
